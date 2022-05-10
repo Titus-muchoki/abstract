cb0f957bb96af6f2cd9d3588a4b2608992ba36ec
kajela <titoyut56@gmail.com>
2022-05-10 05:49:44 -0400

    add the initial files

---
+++ src/Abstract.java
@@ -0,0 +1,18 @@
+
+//abstract class
+abstract class Abstract {
+    public String crush = "joy wambura";
+    public String color = "light skin";
+    public int age = 21;
+
+   public abstract void occupation();//implementation method.
+}
+//sub class inherit from(person)
+    class love extends Abstract{
+        public int meetYear = 2020;
+       public void occupation() {
+       System.out.println("she is kind she gave me her pen on my next exam which was due in 10mnt.");
+        }
+    }
+
+
---
+++ src/second.java
@@ -0,0 +1,11 @@
+public class second {
+    public static void main(String[] args) {
+        love myObj = new love();
+
+        System.out.println("who is my crush: "+myObj.crush);
+        System.out.println("her color is: "+myObj.color);
+        System.out.println("her age is: "+myObj.age);
+        System.out.println("we happened to meet at the start of: "+myObj.meetYear);
+        myObj.occupation();
+    }
+}
